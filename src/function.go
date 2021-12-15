c9212d35da3f74ad75607236a67337883bc98263
chenqi <chenqi1@sensetime.com>
2021-12-15 16:18:24 +0800

    function

--- src/function.go
+++ src/function.go
@@ -5,6 +5,13 @@ func add(x int, y int) int{
 	return x + y
 }
 
+
+func addextra(x, y, z int) int {
+	return x + y + z
+}
+
 func main(){
 	fmt.Println(add(1, 3))
+	fmt.Println(addextra(1, 3, 5))
+	
 }
