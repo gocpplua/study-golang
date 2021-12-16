0ce1edf400e4969e008f35d91f8a833a751ef64f
chenqi <chenqi1@sensetime.com>
2021-12-16 17:19:22 +0800

    if-with-a-short-statement.go

--- src/if-with-a-short-statement.go
+++ src/if-with-a-short-statement.go
@@ -2,7 +2,9 @@ package main
 import "fmt"
 
 func main(){
-	if sum := 0; sum <= 0 {  // var sum = 0 -> error:  var declaration not allowed in if initializer
+	if sum := 1; sum <= 0 {  // var sum = 0 -> error:  var declaration not allowed in if initializer
 		fmt.Println("less than 0")
+	}	else {
+		fmt.Println(sum)
 	}
 }
